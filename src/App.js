f3ff60bdcca880e8eea32e9056a4ed4f0fc934d9
Harshpateloo7 <patelharsh397@gmail.com>
2024-05-02 13:44:09 -0400

    update on navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar/navbar";
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
