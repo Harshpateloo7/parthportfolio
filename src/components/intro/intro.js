83378a65df88da221434f3f763b27be725ceb733
Harshpateloo7 <patelharsh397@gmail.com>
2024-05-05 22:20:44 -0400

    update on intro css and import google fonts style

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,15 +1,18 @@
 import React from "react";
 import './intro.css';
 import bg from '../../assets/profile.png';
+import btnImg from '../../assets/hireMe.png';
+import { Link } from "react-scroll";
+
+
 const Intro = () =>{
     return(
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello</span>
                 <span className="introtext">I'm <span className="introName">Parth</span><br/>Technical Support Specialist</span>
-
-
-
+                <p className="intropara">Empowering your technology journey, one solution at a time.</p>
+                <Link><button className="btn"><img src={btnImg} alt="hire me" />Hire Me</button></Link>
             </div>
             <img src={bg} alt="profile photo" className="bg"></img>
         </section>
